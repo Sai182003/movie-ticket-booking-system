bdd1e8bcba06d57ea3fd6f43458a84a3643962a6
Sai182003 <108660259+Sai182003@users.noreply.github.com>
2023-09-05 17:35:37 +0530

    Add files via upload

---
+++ Theatrelist.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/theatres.css">
+    <title>Threatre List</title>
+</head>
+<body class="style">
+    <h1>THEATRES</h1>
+    <!-- <div class="t-con">
+        <div class="name">
+            <h3 class="t-name">PVR Gachibowli</h3>
+        </div>
+        <div class="timings">
+            <div class="time">
+                10:00 AM
+            </div>
+            <div class="time">
+                12:00 AM
+            </div>
+            <div class="time">
+                01:00 PM
+            </div>
+            <div class="time">
+                03:00 PM
+            </div>
+        </div>
+    </div> -->
+    <div class="container">
+        <a href="#" class="btn">AMB</a>
+        <h5>gachibowli</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">8:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">11:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">3:10</a>
+        </div>
+    </div>
+    <div class="container">
+        <a href="" class="btn">ASIAN M CUBE MALL</a>
+        <h5>attapur</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">9:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">10:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">2:10</a>
+        </div>
+    </div>
+    <div class="container">
+        <a href="" class="btn">DEVI </a>
+        <h5>rtc x roads</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">10:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">11:00</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">1:10</a>
+        </div>
+    </div>
+    <div class="container">
+        <a href="#" class="btn">SUDARSHAN</a>
+        <h5> rtc x roads</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">5:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">7:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">9:10</a>
+        </div>
+    </div>
+    <div class="container">
+        <a href="#" class="btn">PRASAD IMAX</a>
+        <h5>secundrabad</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">8:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">11:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">3:!0</a>
+        </div>
+    </div>
+    <div class="container">
+        <a href="" class="btn">ASIAN MULTIPLEX</a>
+        <h5>Tarnaka</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">8:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">11:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">3:10</a>
+        </div>
+    </div>
+    <div class="container">
+        <a href="" class="btn">BR HItech </a>
+        <h5>hitech city madhapur</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">8:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">11:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">3:10</a>
+        </div>
+    </div>
+    <div class="container">
+        <a href="" class="btn">ASIAN MUKUND CINEMA</a>
+        <h5>Secunderabad</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+             <a href="Theatreseats.php" class="btn2">6:10</a>
+            <!-- <input type="time" class="btn2" value="8:10"> -->
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">10:10</a>
+            <!-- <input type="time" class="btn2" value="8:10"> -->
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">1:10</a>
+        </div>
+    </div>
+    <div class="container">
+        <a href="" class="btn">MIRAJ CINEMA</a>
+        <h5>Secunderabad</h5>
+    </div>
+    <div class="container3">
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">12:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">10:10</a>
+        </div>
+        <div class="container2">
+            <a href="Theatreseats.php" class="btn2">1:10</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ movies.php
@@ -0,0 +1,109 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style copy.css" >
+    <title>Movie booking</title>
+</head>
+<body>
+    <section class="top-bar">
+        <div class="left-content">
+            <h2 class="title">Show Time</h2>
+            <ul class="navigation">
+                <li><a href="movies.php">Home</a></li>
+                <li><a class="#" href="#">movies</a></li>
+                <li><a href="#">theatres</a></li>
+                <li><a href="#">news</a></li>
+            </ul>   
+        </div>
+    </section>
+    <section class="main-container">
+            <div class="current-movies">
+                <div class="current-movie">
+                    <div class="cm-img-box">
+                        <img src="img/movie1.jpg" alt="" class="img" >
+                    </div>
+                    <h3 class="movie-title" value="jurassic world">Jurassic World</h3>
+                    <!-- <p class="screen-name">Screen:Platinum</p> -->
+                    <div class="booking">
+                        <a href="Theatrelist.php" class="btn" >Buy Tickets</a>
+                    </div>
+                </div>
+                <div class="current-movie">
+                    <div class="cm-img-box">
+                        <img src="img/movie2.jpg" alt="" class="img">
+                    </div>
+                    <h3 class="movie-title">vikram</h3>
+                    <!-- <p class="screen-name">Screen:gold</p> -->
+                    <div class="booking">
+                        <a href="Theatrelist.php" class="btn" >Buy Tickets</a>
+                    </div>
+                </div>
+                <div class="current-movie">
+                    <div class="cm-img-box">
+                        <img src="img/RRR.jpeg" alt="" class="img" >
+                    </div>
+                    <h3 class="movie-title">RRR</h3>
+                    <!-- <p class="screen-name">Screen:silver</p> -->
+                    <div class="booking">
+                        <a href="Theatrelist.php" class="btn" >Buy Tickets</a>
+                    </div>
+                </div>
+                <div class="current-movie">
+                    <div class="cm-img-box">
+                        <img src="img/avt.jpeg" alt="" class="img" >
+                    </div>
+                    <h3 class="movie-title">AVATAR</h3>
+                    <!-- <p class="screen-name">Screen:silver</p> -->
+                    <div class="booking">
+                        <a href="Theatrelist.php" class="btn" >Buy Tickets</a>
+                    </div>
+                </div>
+                <div class="current-movie">
+                    <div class="cm-img-box">
+                        <img src="img/dmk.jpg" alt="" class="img" >
+                    </div>
+                    <h3 class="movie-title">Dhamaka</h3>
+                    <!-- <p class="screen-name">Screen:silver</p> -->
+                    <div class="booking">
+                        <a href="Theatrelist.php" class="btn" >Buy Tickets</a>
+                    </div>
+                </div>
+                <div class="current-movie">
+                    <div class="cm-img-box">
+                        <img src="img/oe.jpg" alt="" class="img" >
+                    </div>
+                    <h3 class="movie-title">18 Pages</h3>
+                    <!-- <p class="screen-name">Screen:silver</p> -->
+                    <div class="booking">
+                        <a href="Theatrelist.php" class="btn" >Buy Tickets</a>
+                    </div>
+                </div>
+                <div class="current-movie">
+                    <div class="cm-img-box">
+                        <img src="img/vss.jpeg" alt="" class="img" >
+                    </div>
+                    <h3 class="movie-title">Veera Simha Reddy</h3>
+                    <!-- <p class="screen-name">Screen:silver</p> -->
+                    <div class="booking">
+                        <a href="Theatrelist.php" class="btn" >Buy Tickets</a>
+                    </div>
+                </div>
+                <div class="current-movie">
+                    <div class="cm-img-box">
+                        <img src="img/wvp1.jpeg" alt="" class="img" >
+                    </div>
+                    <h3 class="movie-title">Valteru Verayya</h3>
+                    <!-- <p class="screen-name">Screen:silver</p> -->
+                    <div class="booking">
+                        <a href="Theatrelist.php" class="btn" >Buy Tickets</a>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </section>
+</body>
+</html>
